e544454f1d725f0889c4625e628bdfbd51868553
Mohammad Kafee Uddin <kafee786@gmail.com>
2014-06-24 16:40:10 +0530

    Update Solution1.c

--- Solution1.c
+++ Solution1.c
@@ -3,13 +3,14 @@
 int main(){
     int i, j, sum;
     
-    sum = 0;    
-    for(i = 1;i <= 100){
+    sum = 0;
+    for(i = 1;i < 100;i++){
         if(i%3 == 0 || i%5 == 0){
             sum += i;
         }
     }
+    printf("%d\n", sum);
+    sum = 3 * 33 * 17 + 5 * 19 * 10 - 15 * 3 * 7;
     printf("%d", sum);
     return(0);
-}
-        
+}    
